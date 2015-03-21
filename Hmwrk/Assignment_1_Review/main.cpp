10eaefea7280e040f236eea4ca3670b17699ad85
NKanabolo <nornkable@yahoo.com>
2015-03-20 20:07:28 -0700

    no message

--- Hmwrk/Assignment_1_Review/main.cpp
+++ Hmwrk/Assignment_1_Review/main.cpp
@@ -15,9 +15,9 @@
 using namespace std;
 
 //Function Prototypes
-float celcius(int fhnt);
-void prntArr(string names[], int length);
-int binSearch(string names[], int NUM_NAMES, string trgt); //search for the number 50 in one dimensional array
+float celsius(int fhnt);                                     //converts Fahrenheit to Celsius
+void prntArr(string names[], int length);                    //Prints unsorted string array
+int binSearch(string names[], int NUM_NAMES, string trgt);   //search for the number 50 in one dimensional array
 int prntbinSrch(string names[], int NUM_NAMES, string trgt); //print binary search results
 void bSort(string names[], int NUM_NAMES);
 //Execution Begins Here
@@ -119,23 +119,23 @@ int main(int argc, char** argv) {
                     cout<<"For this year, February has 28 days\n\n";
                 if (month ==3)      //if input=3 then output March
                     cout<<"March has 31 days\n\n";
-                else if (month == 4)//if input=4 then output April
+                else if (month == 4)//else if input=4 then output April
                     cout<<"April has 30 days\n\n";
-                else if (month == 5)//if input=5 then output May
+                else if (month == 5)//else if input=5 then output May
                     cout<<"May has 31 days\n\n";
-                else if (month == 6)//if input=6 then output June
+                else if (month == 6)//else if input=6 then output June
                     cout<<"June has 30 days\n\n";
-                else if (month == 7)//if input=7 then output July
+                else if (month == 7)//else if input=7 then output July
                     cout<<"July has 31 days\n\n";
-                else if (month == 8)//if input=8 then output August
+                else if (month == 8)//else if input=8 then output August
                     cout<<"August has 31 days\n\n";
-                else if (month == 9)//if input=9 then output September
+                else if (month == 9)//else if input=9 then output September
                     cout<<"September has 30 days\n\n";
-                else if (month == 10)//if input=10 then output October
+                else if (month == 10)//else if input=10 then output October
                     cout<<"October has 31 days\n\n";
-                else if (month == 11)//if input=11 then output November
+                else if (month == 11)//else if input=11 then output November
                     cout<<"November has 30 days\n\n";
-                else if (month == 12)//if input=12 then output December
+                else if (month == 12)//else if input=12 then output December
                     cout<<"December has 31 days\n\n";
                 
                 //End problem 3
@@ -145,19 +145,23 @@ int main(int argc, char** argv) {
                 //Problem 4
                 //Declare variables
                 float start, incrRte, days, popGrow;
+                
+                //Inputs
                 cout<<"Enter the starting population: ";
-                cin>>start;
+                cin>>start;     //input the starting population
                 cout<<endl;
                 cout<<"Enter the rate of increase of population as a percent: ";
-                cin>>incrRte;
+                cin>>incrRte;   //input the rate of increase in percent
                 cout<<endl;
                 cout<<"Enter number of days of growth: ";
-                cin>>days;
+                cin>>days;      //input the number of days for growth
                 cout<<endl;
                 
-                if (start >= 2&& incrRte >= 0 && days >= 1){
-                    popGrow= start+(start*(incrRte/100)*days);
-                    cout<<"The population grew to " << popGrow << " organisms!\n\n";
+                if (start >= 2&& incrRte >= 0 && days >= 1){    //if starting populatiuon is greater than or equal to 2, rate of increase 
+                                                                //is greater than or equal to 0, and days of growth greater than 
+                                                                //or equal to 1, then calculate growth
+                    popGrow= start+(start*(incrRte/100)*days);  //calculate population growth
+                    cout<<"The population grew to " << popGrow << " organisms!\n\n";//out put population growth
                 }
                 else
                     cout<<"Incorrect input value(s)\n\n";
@@ -166,43 +170,45 @@ int main(int argc, char** argv) {
             }
             case(5):{
                 //Problem 5
-                for (int fhnt=0;fhnt<=20;fhnt++)
-                cout<<"  "<<fhnt<<"     "<<celcius(fhnt)<<endl;
+                //Declare variables
+                for (int fhnt=0;fhnt<=20;fhnt++)               //loop the Fahrenheit counter
+                cout<<"  "<<fhnt<<"     "<<celsius(fhnt)<<endl;//output Fahrenheit and Celsius conversion equivalent
                 //End problem 5
                 break;
             }
             case(6):{
                 //Problem 6
-                int scount=0, ccount=0, rcount=0;
-                char a[3][30];
-                int b[3][3];
-                ifstream infile;
-                infile.open("RainOrShine.txt");
-                for (int i=0; i<3;i++){
+                //Declare variables
+                int scount=0, ccount=0, rcount=0;   //initialize variables
+                char a[3][30];                      //set month by day array size
+                int b[3][3];                        //set weather report array size
+                ifstream infile;                    //declare file
+                infile.open("RainOrShine.txt");     //open file for data
+                for (int i=0; i<3;i++){             
                     for(int j=0; j<30;j++){
-                        infile>>a[i][j];
+                        infile>>a[i][j];            //loop extraction of data from file into month by day array
                         cout<<a[i][j]<<" ";}
                 cout<<endl;}
                 cout<<endl<<endl<<"   S    C    R\n";
                 for (int i=0;i<3;i++){
                     for (int j=0;j<30;j++){
-                        if (a[i][j] == 'S')
+                        if (a[i][j] == 'S')         //if the array element is an "S" then add one occurrence for "S" to report
                             scount++;
-                        else if (a[i][j] == 'C')
+                        else if (a[i][j] == 'C')    //else if the array element is an "C" then add one occurrence for "C" to report
                             ccount++;
-                        else
+                        else                        //else any other array element is an "R", so add one occurrence for "R" to report
                             rcount++;
                     }
-                    b[i][0]=scount;
-                    b[i][1]=ccount;
-                    b[i][2]=rcount;
-                    scount=0;
-                    ccount=0;
-                    rcount=0;
+                    b[i][0]=scount;//store "S" values
+                    b[i][1]=ccount;//store "C" values
+                    b[i][2]=rcount;//store "R" values
+                    scount=0;      //reset "S" counter
+                    ccount=0;      //reset "C" counter
+                    rcount=0;      //reset "R" counter
                 }
                 for (int i=0;i<3;i++){
                     for (int j=0;j<3;j++){
-                        cout<<setw(4)<<b[i][j]<<" ";
+                        cout<<setw(4)<<b[i][j]<<" ";//loop and output values into each element of weather report array
                     }
                     cout<<endl;
                 }
@@ -212,13 +218,14 @@ int main(int argc, char** argv) {
             case(7):
             {
                 //Problem 7
+                //Declare variables
                  string names[20] = {"Nornubari", "Levin", "Mark", "Caddel", "Bae", 
                  "Terry", "Alex", "Patrick", "Garon", "Aaron",
                  "Sean", "Ricardo", "Rick", "Andres", "Josue", 
-                 "Fernando", "Britney", "Maxwell", "Jamal", "Henry"};
-                 bSort(names, 20);
-                 prntArr(names, 20);
-                 prntbinSrch(names, 20, "Levin");
+                 "Fernando", "Britney", "Maxwell", "Jamal", "Henry"};//declare values of the string
+                 bSort(names, 20);                                   //call bubble sort function for string
+                 prntArr(names, 20);                                 //call print array function
+                 prntbinSrch(names, 20, "Maxwell");                    //call binary search function
                 //End problem 7
                 break;
             }
@@ -231,11 +238,12 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-float celcius(int fhnt)
+float celsius(int fhnt)
 {
+    //Declare variables
     float celTemp;
-    celTemp= (5.0/9.0)*(fhnt-32.0);
-    return celTemp;
+    celTemp= (5.0/9.0)*(fhnt-32.0);//convert Fahrenheit to Celsius
+    return celTemp;                //return converted Celsius temperature
     
 }
 
@@ -251,34 +259,36 @@ void prntArr(string names[], int n)
 
 void bSort(string names[], int n)
 {
-    string temp; //Declare variables
+    //Declare variables
+    string temp;
     for(int i=0;i<n;i++) //sort the one dimensional array
     {
        for(int j=0;j<(n-1);j++) //organize array by looking at each column
-            if(names[j]>names[j+1]) //if first number is greater than the next then switch 
-                            //them and continue across rest of array
+            if(names[j]>names[j+1]) //if first string is greater than the next then switch 
+                                    //them and continue across rest of array
            {
                temp=names[j]; //swap values
                names[j]=names[j+1];
                names[j+1]=temp;
            }
     }
-    for(int i=0;i<n;i++) //print each sorted number
+    for(int i=0;i<n;i++) //print each string sorted
         cout<<names[i]<<" ";
     cout<< endl<< endl;
 }
 
 int binSearch(string names[], int n, string trgt)
 {
-    int mid, lowbound= 0, hghbound=n-1; //Declare variables
+    //Declare variables
+    int mid, lowbound= 0, hghbound=n-1; 
     while (lowbound <= hghbound) 
     {
-         int mid = (lowbound + hghbound)/2; //find middle sorted number in one dimensional array
-         if (names[mid] == trgt) //if selected middle value is the target then it is found
+         int mid = (lowbound + hghbound)/2; //find middle sorted string in one dimensional array
+         if (names[mid] == trgt) //if selected middle string is the target then it is found
              return mid;
          else if (names[mid] < trgt) 
-             lowbound = mid + 1; //if selected middle is less than target, then next number is beginning of next search
-         else hghbound = mid - 1; //if selected middle is more than target, then previous number is end of next search
+             lowbound = mid + 1; //if selected middle is less than target, then next string is beginning of next search
+         else hghbound = mid - 1; //if selected middle is more than target, then previous string is end of next search
     }
       return -1;
     
@@ -286,11 +296,12 @@ int binSearch(string names[], int n, string trgt)
 
 int prntbinSrch(string names[], int n, string trgt)
 {
-    int found; //Declare variables
+    //Declare variables
+    int found; 
     found=binSearch(names, n, trgt); //call binary search function
-    if (found==-1) //if value found is not the returned value then say the number was not found, otherwise, it is found
-        cout << endl << "Levin was not found." << endl;
+    if (found==-1) //if string found is not the returned value then say the string was not found, otherwise, it is found
+        cout << endl << "Maxwell was not found." << endl;
     else
-        cout << endl << "Levin was found."<< endl;
+        cout << endl << "Maxwell was found."<< endl;
         
 }
