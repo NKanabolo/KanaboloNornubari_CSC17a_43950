f0651a9eb50517cf9dd81d8d787718a4365ca985
NKanabolo <nornkable@yahoo.com>
2015-03-29 23:32:50 -0700

    no message

--- Hmwrk/Assignment_2/main.cpp
+++ Hmwrk/Assignment_2/main.cpp
@@ -19,6 +19,11 @@ void arrAllo(int max, int n);//allocate array of integers
 void scores(int scr[], int n);//display scores
 void sortScr(int scr[], int n);//sort scores
 void scrAve(int scr[], int n);//calculate average of scores
+void aveLow(int scr[], int n);//calculate average of scores without lowest score
+void donSort(int *arr[], int n);//sort allocated donations array
+void donArr(const int arr[], int n);//display original donations array
+void donPtr(int *arr[], int n);//display sorted donations array
+void reverse(int *rvsPtr, int n);//put array in reverse order
 //Execution Begins Here
 
 int main(int argc, char** argv) {
@@ -30,9 +35,9 @@ int main(int argc, char** argv) {
         cout<<"1.  Mean, Median, Mode\n";
         cout<<"2.  Gaddis, 8thEd, Chapter 9, Problem 1\n";
         cout<<"3.  Gaddis, 8thEd, Chapter 9, Problem 2\n";
-        cout<<"4.  Gaddis, 8thEd, Chapter 9, Problem \n";
-        cout<<"5.  Gaddis, 8thEd, Chapter 9, Problem \n";
-        cout<<"6.  Gaddis, 8thEd, Chapter 9, Problem \n";
+        cout<<"4.  Gaddis, 8thEd, Chapter 9, Problem 3\n";
+        cout<<"5.  Gaddis, 8thEd, Chapter 9, Problem 6\n";
+        cout<<"6.  Gaddis, 8thEd, Chapter 9, Problem 10\n";
         cout<<"7. Quit the program\n";
         cout << "Enter an integer from 1 to 7 to select an option above: ";
         cin>>m_choice;
@@ -62,7 +67,7 @@ int main(int argc, char** argv) {
                 //Problem 2
                 //Declare Variables
                 int max,n;
-                arrAllo(max, n);
+                arrAllo(max, n);//call allocated array
                 //End problem 2
                 break;
             }
@@ -71,39 +76,63 @@ int main(int argc, char** argv) {
                 //Declare Variables
                 int scr[10];
                 int n=10;
-                 scores(scr, n);
-                 sortScr(scr, n);
-                 scrAve(scr, n);
+                 scores(scr, n);//call scores array
+                 sortScr(scr, n);//call sorted scores array
+                 scrAve(scr, n);//call average of scores array
                 //End problem 3
                 break;
             }
             case(4):{
                 //Problem 4
                 //Declare variables
-                
+                int scr[10];
+                int n=10;
+                scores(scr, n);//call scores array
+                sortScr(scr, n);//call and output sorted scores array
+                aveLow(scr, n);//call and output average of scores calculated without lowest score array
                 //End problem 4
                 break;
             }
             case(5):{
                 //Problem 5
                 //Declare variables
-                
+                const int n = 15;//size of array
+                int donate[n];
+                int *arrPtr[n];//pointer to second array
+                for(int i=0; i<n; i++){
+                cout << "Fill in the "<< i+1 << " score. :";
+                cin >> donate[i];//input up to 15 donations for the array
+                }
+                for (int count = 0; count < n; count++){
+                arrPtr[count] = &donate[count];//make each element point to element in donations array
+                }
+                donSort(arrPtr, n);//call sorted donations array
+                cout << "The donations, sorted in ascending order are: \n";
+                donPtr(arrPtr, n);//output sorted array
+                cout << endl;
+                cout << "The donations, in their original order are: \n"; 
+                donArr(donate, n);//output original donations array
+                cout << endl;
                 //End problem 5
                 break;
             }
             case(6):{
                 //Problem 6
                 //Declare variables
-                
+                 const int n = 10;
+                 int rev[n] = {45, 52, 100, 16, 25, 30, 27, 82, 90, 76};
+                 int *rvsPtr = '\0';
+                 
+                 rvsPtr = rev;
+                 reverse(rvsPtr, n);//call and output original and reverse order array
                 }
                 //End problem 6
                 break;
             
-            
             default:
                 cout<<"Unknown input, please try again.\n\n";
-    }}
-        
+    }
+    }
     //Exit program
     cout << "Now exiting program\n";
     return 0;
@@ -189,11 +218,11 @@ void arrAllo(int max, int n){
         for (n=0; n<max; n++)
         {
           cout << "Enter number: ";
-          cin >> ptr[n];
+          cin >> ptr[n];//enter value for use
         }
         cout << "You have entered: ";
         for (n=0; n<max; n++)
-          cout << ptr[n] << " ";
+          cout << ptr[n] << " ";//display the values entered
         cout<<endl<<endl;
     delete[] ptr;
     }
@@ -236,4 +265,66 @@ void scrAve(int scr[], int n){
         total += scr[i]; //add up each of the 10 scores
     }
     cout << "The average score is a " << total/n << endl <<endl;//calculate and output average
+}
+
+void aveLow(int scr[], int n){
+    //Declare variables
+    int  average, low, total = 0;
+    int score[0];
+    low=score[0];
+    for(int i=0;i<n; i++){
+        total += scr[i]; //add up each of the 10 scores
+        if (scr[i] < low)
+            low = scr[i];//find lowest score
+    }
+    average = (total -  low) / (n-1);//calculate average without the lowest score
+
+    cout << "The average test score is: " << average << endl << endl;//output average score
+    cout << "Lowest is: " << low << endl << endl;//output lowest score
+}
+
+ void donSort(int *arr[], int n){
+     //Declare variables
+     int start, min;
+     int *elmnt;
+     for (start = 0; start < n-1; start++){
+         min = start;//initial i equals start number
+         elmnt = arr[start];//set first element equal to i of array
+         for(int i = start + 1; i < n; i++){
+             if (*(arr[i]) < *elmnt){
+                 elmnt = arr[i];//swap values
+                 min = i;
+             }
+         }
+         arr[min] = arr[start];
+         arr[start] = elmnt;
+     }
+ }
+
+void donArr(const int arr[], int n){
+    for (int count = 0; count < n; count++)
+        cout << arr[count] << " ";//output original donation values
+    cout << endl << endl;
+}   
+
+void donPtr(int *arr[], int n){
+    for (int count = 0; count < n; count++)
+        cout << *(arr[count]) << " ";//output sorted donation values
+    cout << endl << endl;
+} 
+
+void reverse(int *rvsPtr, int n){
+    //Declare variables
+    int count;
+     cout << "The numbers in set are:\n";
+     for (count = 0; count < n; count++){
+         cout << *rvsPtr << " ";//prints numbers in the rev array
+         rvsPtr++;
+     }  
+      cout << "\n\nThe numbers in set backward are:\n";
+      for (count = 0; count < n; count++){
+         rvsPtr--;//decrement order of rev array
+         cout << *rvsPtr << " ";//print rev array in reverse order
+      }
+      cout<<endl<<endl;
 }
